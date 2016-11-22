acf69a61705a14919059f307aed1074e112ca76c
Guanlun Zhao <guanlunzhao@gmail.com>
2016-11-22 16:04:44 -0600

    Walk towards each other.

---
+++ Assets/Fight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Fight : MonoBehaviour {
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = target.position - this.transform.position;
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+        if (direction.magnitude > 1)
+        {
+            this.transform.Translate(0, 0, 0.02f);
+        }
+	}
+}
